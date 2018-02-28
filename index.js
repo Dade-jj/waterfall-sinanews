32cd78e17a54f02793c103453b884ab541d04761
peanut <547806059@qq.com>
2018-02-28 15:47:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $(window).on("scroll",function(){
 
 function loadImg(){
 	$.ajax({
-		url: 'http://platform.sina.com.cn/slide/album_tech',
+		url: 'https://platform.sina.com.cn/slide/album_tech',
 		dataType: 'jsonp',
 		jsonp: "jsoncallback",
 		data: {
